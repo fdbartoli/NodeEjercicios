e0e3f101ecc62a59861d1d9ad6fe7f152f2fe8c6
Fernando Bártoli <54591089+fdbartoli@users.noreply.github.com>
2023-04-15 11:15:29 -0300

    se sube actividad 1

--- app.js
+++ app.js
@@ -2,28 +2,29 @@ const calculadora = require('./calculadora.js')
 
 
 try{
-    console.log("Suma: " + sumar(2,2))
+    console.log("Suma: " + calculadora.sumar(2,2))
 
-    console.log("Suma con error: " +sumar("2",2))
+    console.log("Suma con error: " + calculadora.sumar("2",2))
 
-    console.log("resta: " +restar(10,2))
+    console.log("resta: " + calculadora.restar(10,2))
 
-    console.log("resta con error: " + restar("a",2))
+    console.log("resta con error: " + calculadora.restar("a",2))
 
-    console.log("Multiplicación: " + multiplicar (20, 3))
+    console.log("Multiplicación: " + calculadora.multiplicar (20, 3))
 
-    console.log("Multiplicación con error: " + multiplicar ("a", 3))
+    console.log("Multiplicación con error: " + calculadora.multiplicar ("a", 3))
 
-    console.log("Division: " + dividir(100, 50))
+    console.log("Division: " + calculadora.dividir(100, 50))
 
-    console.log("Division con error: " + dividir(10, "3"))
+    console.log("Division con error: " + calculadora.dividir(10, "3"))
 
-    console.log("Divide por 0" + dividir (10, 0))
+    console.log("Divide por 0" + calculadora.dividir (10, 0))
 
-    console.log("Es par o impar: " + esParOImpar (10))
+    console.log("Es par o impar: " + calculadora.esParOImpar (10))
 
-    console.log("Es par o impar con error: " + esParOImpar(a))
+    console.log("Es par o impar con error: " + calculadora.esParOImpar(a))
     }
 catch (error){
-    console.log("se rompió!!!! " + error.message)
+    console.log(`Error!!!! ${error.message}` )
 }
+
