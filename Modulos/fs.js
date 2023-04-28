bc7db015568bb163f58932e3aa5ee066f2654603
Fernando Bártoli <54591089+fdbartoli@users.noreply.github.com>
2023-04-28 10:29:22 -0300

    21-4

---
+++ Modulos/fs.js
@@ -0,0 +1,31 @@
+const fs = require ('fs')
+//leo un archivo 
+// fs.readFile('indeax.html','UTF-8',(err, cont) => {
+//   if(err){
+//     console.log(err)
+//   }else(
+//     console.log(cont)
+//   )
+//   console.log ("el programa funcionaaaa")
+// }) 
+
+
+//cambio el nombre de un archivo existente
+// fs.rename('index.html', 'indexNuevo.html', (err)=>{
+//     if (err){
+//         throw err;
+//     }
+//     console.log("el archivo ha sido modificado exitosamente")
+// })
+
+
+// fs.appendFile('index.html', 'Contenido agregado usando appendFile', (err,)=>{
+//     if (err) throw err;
+//     console.log("Contenido agregado exitosamente")
+// } )
+
+// writeFile -> creo un archivo o reemplaza en caso de existir
+
+// unlink -> elimina el archivo
+
+
